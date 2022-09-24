1366193c319adfc4bab2fc53ea025e4239521fde
Vadym Kononenko <vadymk95@gmail.com>
2022-09-24 17:29:17 +0300

    master/project/server/create basic server and connected db

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const PORT = process.env.PORT;
+
+const app = express();
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+start();
