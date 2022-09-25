c87db2b630ece358bd7fdf746ac8e8f928cb358e
Vadym Kononenko <vadymk95@gmail.com>
2022-09-25 17:50:33 +0300

    master/server/added based routes and error processing

--- server/index.js
+++ server/index.js
@@ -3,12 +3,19 @@ const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
 const cors = require('cors');
+const router = require('./routes/index');
+const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+
 const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
+
+//*middleware with error always in the end of uses
+app.use(errorHandler);
 
 const start = async () => {
   try {
