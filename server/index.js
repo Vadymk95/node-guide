a8ccad497a74187e240a10c1ad3aecd73e9e271b
Vadym Kononenko <vadymk95@gmail.com>
2022-09-26 14:41:01 +0300

    master/server/feat/controllers/finished deviceController

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const sequelize = require('./db');
 const models = require('./models/models');
+const fileUpload = require('express-fileupload');
 const cors = require('cors');
 const router = require('./routes/index');
 const errorHandler = require('./middleware/ErrorHandlingMiddleware');
+const path = require('path');
 
 const PORT = process.env.PORT;
 
@@ -12,6 +14,8 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(fileUpload({}));
 app.use('/api', router);
 
 //*middleware with error always in the end of uses
