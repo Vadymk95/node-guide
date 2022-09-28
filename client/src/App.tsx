fd87cb9392c5f8af3a04cb15e037d9a3db4480e9
Vadym Kononenko <vadymk95@gmail.com>
2022-09-28 15:43:11 +0300

    master/client/feat/NavBar/created NavBar with react-bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,13 @@
 import { FC } from 'react';
+import { NavBar } from './components';
 import { AppRouter } from './router';
 
 const App: FC = () => {
   return (
-    <div className="app">
-      <h1>Hello world!</h1>
+    <>
+      <NavBar />
       <AppRouter />
-    </div>
+    </>
   );
 };
 
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,37 @@
+import { observer } from 'mobx-react-lite';
 import { FC, useContext } from 'react';
+import { Navbar, Container, Button, Nav } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import { Context } from '../main';
+import { EnumRoutes } from '../router/EnumRoutes';
 
-export const Navbar: FC = () => {
+export const NavBar: FC = observer(() => {
   const { user } = useContext(Context);
-  return <div>Navbar</div>;
-};
+  const handleAuth = () => user.setIsAuth(true);
+  return (
+    <Navbar bg="dark" variant="dark">
+      <Container>
+        <NavLink
+          style={{ color: '#fff', textDecoration: 'none', marginRight: '24px' }}
+          to={EnumRoutes.SHOP_ROUTE}
+        >
+          Buy Device
+        </NavLink>
+        <Nav className="ml-auto">
+          {user.isAuth ? (
+            <>
+              <Button style={{ marginRight: '10px' }} variant="outline-light">
+                Admin Panel
+              </Button>
+              <Button variant="outline-light">Login</Button>
+            </>
+          ) : (
+            <Button onClick={handleAuth} variant="outline-light">
+              Authorization
+            </Button>
+          )}
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+});
