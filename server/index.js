b78bcf72965d4fca94dcb2fe235e38133836baf3
Vadym Kononenko <vadymk95@gmail.com>
2022-09-30 16:40:56 +0300

    master/server/feat/index/added express middleware

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload({}));
